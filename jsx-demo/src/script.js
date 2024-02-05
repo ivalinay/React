d6826ccaeb5ecf940d0e61f1d53a5f69a9a06971
ivalina <ivalina757@gmail.com>
2024-02-05 17:21:56 +0200

    Updated React and React DOM libraries

--- jsx-demo/src/script.js
+++ jsx-demo/src/script.js
@@ -1,2 +1,2 @@
 import "../node_modules/react/umd/react.production.min.js";
-import "../node_modules/react-dom/umd/react.production.min.js"
+import "../node_modules/react-dom/umd/react-dom.production.min.js"
