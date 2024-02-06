552af19a6fbb31e780449b0ca18ea461597a2c86
ivalina <ivalina757@gmail.com>
2024-02-06 18:36:39 +0200

    Initialize React rendering with a basic heading

--- jsx-demo/src/script.js
+++ jsx-demo/src/script.js
@@ -1,8 +1,10 @@
 import "../node_modules/react/umd/react.production.min.js";
-import "../node_modules/react-dom/umd/react-dom.production.min.js"
+import "../node_modules/react-dom/umd/react-dom.production.min.js";
 
-const rootDomElement = document.getElementById('root')
+const rootDomElement = document.getElementById("root");
 
-const root = ReactDOM.createRoot(rootDomElement)
+const root = ReactDOM.createRoot(rootDomElement);
 
-const reactHeadingElement = 
+const reactHeadingElement = React.createElement("h1", {}, "Hello from JSX!");
+
+root.render(reactHeadingElement);
