fae39a15a68f07d852be0aea0a1c91c5cb6be154
ivalina <ivalina757@gmail.com>
2024-02-06 14:53:06 +0200

    Added React and ReactDOM production builds

--- jsx-demo/src/script.js
+++ jsx-demo/src/script.js
@@ -1,2 +1,8 @@
 import "../node_modules/react/umd/react.production.min.js";
 import "../node_modules/react-dom/umd/react-dom.production.min.js"
+
+const rootDomElement = document.getElementById('root')
+
+const root = ReactDOM.createRoot(rootDomElement)
+
+const reactHeadingElement = 
