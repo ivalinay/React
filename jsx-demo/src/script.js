84a3b100acfcb4a0d286e10263b13ac597ddb6cc
ivalina <ivalina757@gmail.com>
2024-02-07 18:46:46 +0200

    Add secondary heading and wrap headings in header element

--- jsx-demo/src/script.js
+++ jsx-demo/src/script.js
@@ -6,5 +6,7 @@ const rootDomElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootDomElement);
 
 const reactHeadingElement = React.createElement("h1", {}, "Hello from JSX!");
+const reactSecondHeading = React.createElement("h2", {}, "The best syntax ever!");
+const header = React.createElement("header", { classname: "site-header" }, reactHeadingElement, reactSecondHeading);
 
-root.render(reactHeadingElement);
+root.render(header);
