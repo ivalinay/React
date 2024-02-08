a85a220690e311a3951e2cdea5d6422779fdc07a
ivalina <ivalina757@gmail.com>
2024-02-08 13:33:06 +0200

    Editet className

--- jsx-demo/src/script.js
+++ jsx-demo/src/script.js
@@ -5,8 +5,9 @@ const rootDomElement = document.getElementById("root");
 
 const root = ReactDOM.createRoot(rootDomElement);
 
-const reactHeadingElement = React.createElement("h1", {}, "Hello from JSX!");
+const reactHeading = React.createElement("h1", {}, "Hello from JSX!");
 const reactSecondHeading = React.createElement("h2", {}, "The best syntax ever!");
-const header = React.createElement("header", { classname: "site-header" }, reactHeadingElement, reactSecondHeading);
+const header = React.createElement("header", { className: "site-header" }, reactHeading, reactSecondHeading,
+);
 
 root.render(header);
