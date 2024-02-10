d432e226f435d87d568976af1b29bfb4a926d8f5
ivalina <ivalina757@gmail.com>
2024-02-10 17:03:29 +0200

    Created new folders

---
+++ jsx-demo/build/script.js
@@ -0,0 +1,32 @@
+import "../node_modules/react/umd/react.production.min.js";
+import "../node_modules/react-dom/umd/react-dom.production.min.js";
+
+var rootDomElement = document.getElementById("root");
+
+var root = ReactDOM.createRoot(rootDomElement);
+
+// const reactHeading = React.createElement("h1", {}, "Hello from JSX!");
+// const reactSecondHeading = React.createElement("h2", {}, "The best syntax ever!");
+// const header = React.createElement("header", { className: "site-header" }, reactHeading, reactSecondHeading);
+
+var headerJSX = React.createElement(
+  "header",
+  { className: "site-header" },
+  React.createElement(
+    "h1",
+    null,
+    "Hello from JSX!"
+  ),
+  React.createElement(
+    "h2",
+    null,
+    "The best syntax ever!"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "something else here"
+  )
+);
+
+root.render(headerJSX);
