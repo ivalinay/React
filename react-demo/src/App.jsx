f71546bb5bac23e26138c28f5c757e87f6b44a0b
ivalina <ivalina757@gmail.com>
2024-02-13 12:22:48 +0100

    Create basic structure for React app with Vite

--- react-demo/src/App.jsx
+++ react-demo/src/App.jsx
@@ -2,14 +2,14 @@
 import "./App.css";
 
 function App() {
-
   return (
-    <>
+    <div>
       <div>
         <h1>My first react application</h1>
       </div>
+
       <h1>Vite + React = best combo </h1>
-    </>
+    </div>
   );
 }
 
