8c7eed9b5f7309d525cfff3dcc54a7168edb29cb
ivalina <ivalina757@gmail.com>
2024-02-22 12:20:31 +0100

    Apartment Water House Website project

---
+++ Apartment Water House Website Template/src/App.jsx
@@ -0,0 +1,25 @@
+import Header from "./components/Header";
+import OurServices from "./components/OurServices";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import WhyUs from "./components/WhyUs";
+import OurTeam from "./components/OurTeam";
+import Client from "./components/Client";
+import Info from "./components/Info";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <OurServices />
+      <About />
+      <WhyUs />
+      <OurTeam />
+      <Client />
+      <Info />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
