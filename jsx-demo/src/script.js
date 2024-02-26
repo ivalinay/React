622a273f4c83c30410597dfda938617a51343dc1
ivalina <ivalina757@gmail.com>
2024-02-26 13:31:00 +0100

    Successfully removed old Footer component, transitioned to JSX syntax.

--- jsx-demo/src/script.js
+++ jsx-demo/src/script.js
@@ -5,27 +5,7 @@ const rootDomElement = document.getElementById("root");
 
 const root = ReactDOM.createRoot(rootDomElement);
 
-// const reactHeading = React.createElement("h1", {}, "Hello from JSX!");
-// const reactSecondHeading = React.createElement("h2", {}, "The best syntax ever!");
-// const header = React.createElement("header", { className: "site-header" }, reactHeading, reactSecondHeading);
 
-// const Footer = () => {
-//   return React.createElement(
-//     "div",
-//      { className: "site-footer" },
-//       React.createElement(
-//         "p",
-//          {},
-//           "All rights reserved"
-//           )
-//           );
-// };
-
-const Footer = () => {
-  <div className="site-footer">
-    <p>All rights reserved &copy;</p>
-  </div>;
-};
 
 const headerJSX = (
   <div>
