9ab8ab020b49af1adb1924f6323d428ec2780a94
ivalina <ivalina757@gmail.com>
2024-02-29 15:22:31 +0100

    Components Basic idea -props and state

--- 02.ReactJS-Components-Basic-Idea/02.ReactJS-Components-Basic-Idea/components-basics/src/components/Movie.jsx
+++
@@ -1,9 +0,0 @@
-export default function Movie(props) {
-    return (
-        <li>
-            <h3>{props.movieData.title}</h3>    
-            <p>Year: {props.movieData.year}</p>
-            <p>Cast: {props.movieData.actors}</p>
-        </li>
-    );
-}
---
+++ 02.ReactJS-Components-Basic-Idea/components-basic/src/App.jsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <div>My first Dynamic React Application</div>
+  )
+}
+
+export default App
