2ccf961de6552909ec7efd8b878a800c0e4023d8
ivalina <ivalina757@gmail.com>
2024-03-01 11:30:13 +0100

    Integrated MovieList component with props for dynamic content

--- 02.ReactJS-Components-Basic-Idea/components-basic/src/App.jsx
+++ 02.ReactJS-Components-Basic-Idea/components-basic/src/App.jsx
@@ -1,12 +1,28 @@
+import MovieList from "./components/MovieList";
+import "./App.css";
 
-import './App.css'
+const movies = [
+  {
+    title: "The Matrix",
+    description: "Description here",
+  },
+  {
+    title: "Man of Steel",
+    description: "Description here",
+  },
+  {
+    tittle: "Harry Potter",
+    description: "Description here",
+  },
+];
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-  <div>My first Dynamic React Application</div>
-  )
+    <div>
+      <h1>My first Dynamic React Application</h1>
+      <MovieList headingText="Filmi" secondaryText="Secondary Text" />
+    </div>
+  );
 }
 
-export default App
+export default App;
