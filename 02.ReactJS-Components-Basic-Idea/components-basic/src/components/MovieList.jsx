b94388d588f95f70c51eb6c86f81ea77a886977a
ivalina <ivalina757@gmail.com>
2024-03-03 12:54:37 +0200

    Updated components props

--- 02.ReactJS-Components-Basic-Idea/components-basic/src/components/MovieList.jsx
+++ 02.ReactJS-Components-Basic-Idea/components-basic/src/components/MovieList.jsx
@@ -1,11 +1,11 @@
 export default function MovieList(props) {
   return (
     <div className="movie-list">
-      <h2>{props.headingText}</h2>
 
       <ul>
         <li>First movie</li>
         <li>Second movie</li>
+        <li>Third movie</li>
       </ul>
     </div>
   );
