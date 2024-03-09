ca57255fdb00c60b9f6296257b445e70e01c4720
ivalina <ivalina757@gmail.com>
2024-03-09 16:25:48 +0200

    Deleted props component

--- 02.ReactJS-Components-Basic-Idea/components-basic/src/components/Movie.jsx
+++ 02.ReactJS-Components-Basic-Idea/components-basic/src/components/Movie.jsx
@@ -1,9 +1,8 @@
-export default function  Movie(props){
-    return (
-        <article>
-           <h3>{props.title}</h3>
-           <p>Year: {props.data.year}</p>
-           <p>Cast: {props.data.actor}</p>
-        </article>
-    );
+export default function Movie(props) {
+  return (
+    <article>
+      <h3>{props.title}</h3>
+      <p>Year: {props.data.year}</p>
+    </article>
+  );
 }
