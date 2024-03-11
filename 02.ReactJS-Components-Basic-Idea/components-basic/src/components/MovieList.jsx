e7a03a866b0956ae844ec387de97264889339723
ivalina <ivalina757@gmail.com>
2024-03-11 18:27:37 +0200

    Added component for heading with support of children for flexible titles

--- 02.ReactJS-Components-Basic-Idea/components-basic/src/components/MovieList.jsx
+++ 02.ReactJS-Components-Basic-Idea/components-basic/src/components/MovieList.jsx
@@ -1,18 +1,15 @@
 import Movie from "./Movie";
+import Heading from "./Heading";
 
 export default function MovieList(props) {
   return (
-    <div className="movie-list">
-      <h2>{props.headingText}</h2>
-
+    <div className="movie-list"> 
+    <Heading>{props.headingText}</Heading>
       <ul>
-        <li>
-          <li>
-            <Movie data={props.movies[0]} />
-            <Movie data={props.movies[1]} />
-            <Movie data={props.movies[2]} />
-          </li>
-        </li>
+        <Movie data={props.movies[0]} />
+        <Movie data={props.movies[1]} />
+        <Movie data={props.movies[2]} />
+        <Movie data={props.movies[3]} />
       </ul>
     </div>
   );
