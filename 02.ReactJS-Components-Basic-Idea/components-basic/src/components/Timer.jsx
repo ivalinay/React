795caceb7e7b55e973109d60824aadb07926b464
ivalina <ivalina757@gmail.com>
2024-03-15 18:29:05 +0200

    Implemented stateful Timer component with interval

--- 02.ReactJS-Components-Basic-Idea/components-basic/src/components/Timer.jsx
+++ 02.ReactJS-Components-Basic-Idea/components-basic/src/components/Timer.jsx
@@ -1,14 +1,15 @@
 export default function Timer(props) {
-let time = 0; 
+  let time = 0;
 
+  setTimeout(() => {
+    time++;
+  }, 2000);
 
+  return (
+    <div>
+      <h3>Timer</h3>
 
-return (
-<div>
-    <h3>Timer</h3>
-    
-
-    <p>{time}</p>
-</div>
-);
+      <p>{time}</p>
+    </div>
+  );
 }
