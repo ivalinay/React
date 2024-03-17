b337ef2e4747a5ce3cf5ab1f5850d367598c22c4
ivalina <ivalina757@gmail.com>
2024-03-17 13:27:19 +0200

    Improved code

--- 02.ReactJS-Components-Basic-Idea/components-basic/src/components/Timer.jsx
+++ 02.ReactJS-Components-Basic-Idea/components-basic/src/components/Timer.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 export default function Timer(props) {
   const [time, setTime] = useState(0); // Pass initial state 
   // let time = 0;
+  
   console.log(`Current time is = ${time}`);
 
   setTimeout(() => {
