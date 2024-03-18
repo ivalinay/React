b2a1175f9e36777d0bae90bcadd338cff9702335
ivalina <ivalina757@gmail.com>
2024-03-18 19:39:59 +0200

    Added increment functionality to Counter component

--- 02.ReactJS-Components-Basic-Idea/components-basic/src/components/Counter.jsx
+++ 02.ReactJS-Components-Basic-Idea/components-basic/src/components/Counter.jsx
@@ -3,11 +3,16 @@ import { useState } from "react";
 export default function Counter(props) {
   const [count, setCount] = useState(0);
 
+  const incrementClickHandler = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div>
       <h1> Counter</h1>
 
       <p> Count: {count}</p>
+      <button onClick={incrementClickHandler}>+</button>
     </div>
   );
 }
