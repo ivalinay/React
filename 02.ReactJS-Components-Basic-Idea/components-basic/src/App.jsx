10c0cead2d340f258fa157c86dabd863533eb905
ivalina <ivalina757@gmail.com>
2024-03-18 12:58:01 +0200

    Transitioned Timer into the useEffect dimension for time updates

--- 02.ReactJS-Components-Basic-Idea/components-basic/src/App.jsx
+++ 02.ReactJS-Components-Basic-Idea/components-basic/src/App.jsx
@@ -7,9 +7,7 @@ function App() {
   return (
     <div>
       <h1>My first Dynamic React Application</h1>
-      <Timer startTime ={5} />
-      <Timer startTime = {6}/>
-      <Timer startTime = {7} />
+      <Timer startTime={5} />
 
       <MovieList movies={movies} headingText="Movie List" />
     </div>
--- 02.ReactJS-Components-Basic-Idea/components-basic/src/components/Timer.jsx
+++ 02.ReactJS-Components-Basic-Idea/components-basic/src/components/Timer.jsx
@@ -1,15 +1,13 @@
 import { useState } from "react";
 
 export default function Timer(props) {
-  const [time, setTime] = useState(props.startTime); // Pass initial state 
-  // let time = 0;
-  
+  const [time, setTime] = useState(props.startTime); // Pass initial state
+
   console.log(`Current time is = ${time}`);
 
   // Note: Don't use setTimeout,useEffect is more appropriate
   setTimeout(() => {
     setTime(time + 1);
-
   }, 1000);
 
   return (
