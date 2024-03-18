0182d662cc23e50227dd25b7f7f0c3c2fe624ee1
ivalina <ivalina757@gmail.com>
2024-03-18 19:30:24 +0200

    Implemented basic Counter component setup with useState hook

---
+++ 02.ReactJS-Components-Basic-Idea/components-basic/src/components/Counter.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+export default function Counter(props) {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1> Counter</h1>
+
+      <p> Count: </p>
+    </div>
+  );
+}
