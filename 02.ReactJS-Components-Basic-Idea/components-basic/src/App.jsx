902baa0317311967db3c3f82f603143a1af1a395
ivalina <ivalina757@gmail.com>
2024-03-19 11:52:19 +0200

    Imported Counter from folders

--- 02.ReactJS-Components-Basic-Idea/components-basic/src/App.jsx
+++ 02.ReactJS-Components-Basic-Idea/components-basic/src/App.jsx
@@ -1,12 +1,16 @@
 import MovieList from "./components/MovieList";
 import Timer from "./components/Timer";
 import movies from "./assets/movies";
+import Counter from "./components/Counter";
 import "./App.css";
 
 function App() {
   return (
     <div>
       <h1>My first Dynamic React Application</h1>
+
+      <Counter />
+
       <Timer startTime={5} />
 
       <MovieList movies={movies} headingText="Movie List" />
--- 02.ReactJS-Components-Basic-Idea/components-basic/src/components/Counter.jsx
+++ 02.ReactJS-Components-Basic-Idea/components-basic/src/components/Counter.jsx
@@ -7,12 +7,17 @@ export default function Counter(props) {
     setCount(count + 1);
   };
 
+  const decrementClickHandler = () => {
+    setCount(count - 1);
+  };
+
   return (
     <div>
       <h1> Counter</h1>
 
       <p> Count: {count}</p>
       <button onClick={incrementClickHandler}>+</button>
+      <button onClick={decrementClickHandler}>-</button>
     </div>
   );
 }
