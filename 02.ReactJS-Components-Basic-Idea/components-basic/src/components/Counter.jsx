ef63a896dae216bb4b3fddd58907291adc7c4eb4
ivalina <ivalina757@gmail.com>
2024-03-19 21:29:44 +0200

    Added conditional rendering for invalid count warning

--- 02.ReactJS-Components-Basic-Idea/components-basic/src/components/Counter.jsx
+++ 02.ReactJS-Components-Basic-Idea/components-basic/src/components/Counter.jsx
@@ -11,13 +11,26 @@ export default function Counter(props) {
     setCount(count - 1);
   };
 
-  const clearCounterHandler = () => {
+  const clearCounterHandler = (event) => {
+    console.log(event);
     setCount(0);
   };
 
+  //   if (count < 0) {
+  //     return <h3>Invalid count!</h3>;
+  //   }
+
+  let warning = null;
+
+  if (count < 0) {
+    warning = <p>Invalid count!</p>;
+  }
+
   return (
     <div>
-      <h1> Counter</h1>
+      <h3> Counter</h3>
+
+      {warning}
 
       <p> Count: {count}</p>
       <button onClick={decrementClickHandler}>-</button>
