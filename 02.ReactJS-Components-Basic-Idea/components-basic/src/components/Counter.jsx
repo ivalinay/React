060a5dd41190c08b2653529c776ecf32b00a8b95
ivalina <ivalina757@gmail.com>
2024-03-21 14:34:17 +0200

    Fixed - corrected conditional rendering and added zero count message

--- 02.ReactJS-Components-Basic-Idea/components-basic/src/components/Counter.jsx
+++ 02.ReactJS-Components-Basic-Idea/components-basic/src/components/Counter.jsx
@@ -7,27 +7,21 @@ export default function Counter(props) {
     setCount(count + 1);
   };
 
-  const decrementClickHandler = () => {
-    setCount(count - 1);
-  };
-
   const clearCounterHandler = (event) => {
     console.log(event);
     setCount(0);
   };
 
-  //   if (count < 0) {
-  //     return <h3>Invalid count!</h3>;
-  //   }
-
   return (
     <div>
-      <h3> Counter</h3>
+      <h3>Counter</h3>
 
-      {count < 0 ? <p>Invalid count!</p> : null}
+      {count < 0 ? <p>Invalid count!</p> : <p>Valid count!</p>}
+      {count === 0 && <p>Please start incrementing</p>}
 
-      <p> Count: {count}</p>
-      <button onClick={decrementClickHandler}>-</button>
+      <p>Count: {count}</p>
+
+      <button onClick={() => setCount(count - 1)}>-</button>
       <button onClick={clearCounterHandler}>clear</button>
       <button onClick={incrementClickHandler}>+</button>
     </div>
