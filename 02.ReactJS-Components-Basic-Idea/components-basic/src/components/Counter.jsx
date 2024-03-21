56146cdc671c8a1543b462347fa555aed1133fcb
ivalina <ivalina757@gmail.com>
2024-03-21 17:14:17 +0200

    Fixed syntax error in decrement button onClick handler

--- 02.ReactJS-Components-Basic-Idea/components-basic/src/components/Counter.jsx
+++ 02.ReactJS-Components-Basic-Idea/components-basic/src/components/Counter.jsx
@@ -12,16 +12,50 @@ export default function Counter(props) {
     setCount(0);
   };
 
+  // if(count<0){
+  //   return (
+  //     <h3>Invalid count!</h3>
+  //   );
+  // }
+
+  let message = null;
+
+  switch (count) {
+    case 1:
+      message = "First blood";
+      break;
+    case 2:
+      message = "Double kill";
+      break;
+    case 3:
+      message = "Triple kill";
+      break;
+    case 4:
+      message = "Multi kill";
+      break;
+    case 5:
+      message = "Unstoppable";
+      break;
+    case 6:
+      message = "God like";
+      break;
+    default:
+      message = "Monster kill";
+  }
+
   return (
     <div>
       <h3>Counter</h3>
 
       {count < 0 ? <p>Invalid count!</p> : <p>Valid count!</p>}
+
       {count === 0 && <p>Please start incrementing</p>}
+
+      <h4>{message}</h4>
 
       <p>Count: {count}</p>
 
-      <button onClick={() => setCount(count - 1)}>-</button>
+      <button disabled={count < 0} onClick={() => setCount(count - 1)}>-</button>
       <button onClick={clearCounterHandler}>clear</button>
       <button onClick={incrementClickHandler}>+</button>
     </div>
