85b0b8b32a10e6a6531e8624b1eff4460a2ff95d
ivalina <ivalina757@gmail.com>
2024-04-05 12:58:32 +0300

    Added props in project Apartment

---
+++ Apartment Water House Website Template/src/components/homePage/FeatureList.jsx
@@ -0,0 +1,12 @@
+import FeaturedSection from "./FeaturedSection";
+import FeaturedList from './FeatureList';
+
+export default function FeaturedList(props){
+    return (
+        <div className = 'info-table'>
+<ul>
+    <FeaturedList
+</ul>
+        </div>
+    )
+}
