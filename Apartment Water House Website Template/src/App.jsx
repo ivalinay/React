70dbfca391bb01878b4c2ba46822df3833b88f1a
ivalina <ivalina757@gmail.com>
2024-04-17 16:38:25 +0300

    Removed code

--- Apartment Water House Website Template/src/App.jsx
+++ Apartment Water House Website Template/src/App.jsx
@@ -8,7 +8,6 @@ import VideoSection from "./components/homePage/VideoSection";
 import VideoComponent from "./components/homePage/VideoComponent";
 import FunFacts from "./components/homePage/FunFacts";
 import SectionBestDeal from "./components/homePage/SectionBestDeal";
-import PropertiesSection from "./components/homePage/PropertiesSection";
 import ContactSection from "./components/homePage/ContactSection";
 import ContactContent from "./components/homePage/ContactContent";
 import Footer from "./components/Footer";
@@ -25,7 +24,6 @@ function App() {
       <VideoComponent />
       <FunFacts />
       <SectionBestDeal />
-      <PropertiesSection />
       <ContactSection />
       <ContactContent />
       <Footer />
