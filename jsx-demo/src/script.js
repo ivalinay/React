a5957651ef6afa76fb20ac09e1917251b0d1c9a3
ivalina <ivalina757@gmail.com>
2024-06-18 16:35:42 +0300

    Deleted code from project jsx-demo

--- jsx-demo/src/script.js
+++ jsx-demo/src/script.js
@@ -5,26 +5,6 @@ const rootDomElement = document.getElementById("root");
 
 const root = ReactDOM.createRoot(rootDomElement);
 
-// none  JSX component
+const reactHeadingElement = React.createElement("h1", {}, "Hello From JSX!");
 
-function FooterText() {
-  return React.createElement("p", {}, "All rights reserved  &copy ");
-}
-
-function Footer() {
-  return React.createElement("div", { className: "site-footer" }, React.createElement(FooterText));
-}
-
-const headerJSX = (
-  <div>
-    <header className="site-header">
-      <h1>Hello from JSX!</h1>
-      <h2>The best syntax ever!</h2>
-
-      <p>something else here</p>
-    </header>
-    <Footer />
-  </div>
-);
-
-root.render(headerJSX);
+root.render(reactHeadingElement);
