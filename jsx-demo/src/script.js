78c1484fd9350264d23279889b5107a68a8d9392
ivalina <ivalina757@gmail.com>
2024-06-18 22:07:59 +0300

    Package.json - installed  babel preset-react

--- jsx-demo/src/script.js
+++ jsx-demo/src/script.js
@@ -5,12 +5,17 @@ const rootDomElement = document.getElementById("root");
 
 const root = ReactDOM.createRoot(rootDomElement);
 
-const reactHeading = React.createElement("h1", {}, "Hello From JSX!");
-const reactSecondHeading = React.createElement("h2", {}, "The best syntax ever!");
-const header = React.createElement(
-  "header",
-  { className: "site-header" },
-  reactHeading,
-  reactSecondHeading,);
+// const reactHeading = React.createElement("h1", {}, "Hello From JSX!");
+// const reactSecondHeading = React.createElement("h2", {}, "The best syntax ever!");
+// const header = React.createElement("header", { className: "site-header" }, reactHeading, reactSecondHeading);
 
-root.render(header);
+const headerJSX = (
+  <header className="site-header">
+    <h1>Hello from JSX!</h1>
+    <h2>The best syntax ever!</h2>
+
+    <p>something else here</p>
+  </header>
+);
+
+root.render(headerJSX);
