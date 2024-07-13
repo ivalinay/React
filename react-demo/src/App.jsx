2f1dc6313e5cc81fa12147d87fea2b2f49f07c7a
ivalina <ivalina757@gmail.com>
2024-07-13 15:26:11 +0200

    Deleted Footer component from cryptocurrency project

--- react-demo/src/App.jsx
+++ react-demo/src/App.jsx
@@ -1,11 +1,10 @@
+import About from "./components/About";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import OurServices from "./components/OurServices";
-import Footer from "./components/Footer";
-import About from "./components/About";
-import WhyUs from "./components/WhyUs";
-import OurTeam from "./components/OurTeam";
 import Info from "./components/Info";
+import OurServices from "./components/OurServices";
+import OurTeam from "./components/OurTeam";
+import WhyUs from "./components/WhyUs";
 
 function App() {
   return (
