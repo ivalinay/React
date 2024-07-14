11a146e0de76f2c79bec38903c2d3f3e3669f926
ivalina <ivalina757@gmail.com>
2024-07-14 18:36:32 +0200

    Fixed code

--- react-demo/src/App.jsx
+++ react-demo/src/App.jsx
@@ -1,12 +1,13 @@
-import About from './components/About';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Info from './components/Info';
-import OurServices from './components/OurServices';
-import OurTeam from './components/OurTeam';
-import WhyUs from './components/WhyUs';
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Info from "./components/Info";
+import OurServices from "./components/OurServices";
+import OurTeam from "./components/OurTeam";
+import WhyUs from "./components/WhyUs";
 
 function App() {
+  
   return (
     <div>
       <Header />
