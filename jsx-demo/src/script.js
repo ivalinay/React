ff097a66618bf21102a254aa34d025bcc5e1d5c6
ivalina <ivalina757@gmail.com>
2024-08-12 19:29:59 +0300

    Updated code

--- jsx-demo/src/script.js
+++ jsx-demo/src/script.js
@@ -3,7 +3,6 @@ import "../node_modules/react-dom/umd/react-dom.production.min.js";
 
 const rootDomElement = document.getElementById("root");
 
-
 const root = ReactDOM.createRoot(rootDomElement);
 // none JSX component
 function FooterText() {
