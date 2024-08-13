3bb1dabcd2463c8c07e3f4548ef66e083cba598e
ivalina <ivalina757@gmail.com>
2024-08-13 21:46:03 +0300

    Improved code

--- jsx-demo/src/script.js
+++ jsx-demo/src/script.js
@@ -1,6 +1,7 @@
 import "../node_modules/react/umd/react.production.min.js";
 import "../node_modules/react-dom/umd/react-dom.production.min.js";
 
+
 const rootDomElement = document.getElementById("root");
 
 const root = ReactDOM.createRoot(rootDomElement);
